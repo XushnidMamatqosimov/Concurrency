f8c69b96a03f81086e3a35d98c4e8df9f37b0b5a
Xushnid <xmamtqosimov@gmail.com>
2024-12-06 20:48:45 +0900

    bu test file

---
+++ Homework/gitAndGitHub/Main.java
@@ -0,0 +1,4 @@
+package gitAndGitHub;
+
+public class Main {
+}
---
+++ Homework/gitAndGitHub/Tas1/Main.java
@@ -0,0 +1,7 @@
+package gitAndGitHub.Tas1;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("this is new project for git");
+    }
+}
