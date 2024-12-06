8898dff2ffff81b0ae2a32aa6136df049ae24c12
Xushnid <xmamtqosimov@gmail.com>
2024-12-06 21:03:37 +0900

    new changes are modified

--- Homework/gitAndGitHub/Tas1/Main.java
+++ Homework/gitAndGitHub/Tas1/Main.java
@@ -3,5 +3,6 @@ package gitAndGitHub.Tas1;
 public class Main {
     public static void main(String[] args) {
         System.out.println("this is new project for git");
+        System.out.println("Hello world");
     }
 }
