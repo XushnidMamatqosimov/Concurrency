4ff08a0aede035ac9b551ac12ede13de8ab2120f
Xushnid <xmamtqosimov@gmail.com>
2024-12-18 23:09:08 +0900

    new one

--- review/Mains.java
+++ review/Mains.java
@@ -2,5 +2,6 @@ public class Mains {
     public static void main(String[] args) {
         System.out.println("This directory is created for checking and reviewing all the lessons of module 4, especially Concurrency and IO/NIO, Regex");
         System.out.println("hello");
+        System.out.println("main");
     }
 }
