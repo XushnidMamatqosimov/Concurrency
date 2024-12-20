1aa145e5df72ed63d936ae63654f9bc1de8b896b
Xushnid <xmamtqosimov@gmail.com>
2024-12-20 16:12:15 +0900

    reabase add

--- review/Mains.java
+++ review/Mains.java
@@ -3,6 +3,6 @@ public class Mains {
         System.out.println("This directory is created for checking and reviewing all the lessons of module 4, especially Concurrency and IO/NIO, Regex");
         System.out.println("hello");
         System.out.println("main");
-        System.out.println("rebase");
+        System.out.println("rebase and ");
     }
 }
